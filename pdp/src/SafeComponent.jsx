13259eeb1ae65e6a74bcedf6714631c8df74a0c4
Mourad ElSheraey <mourad.elsheraey@gmail.com>
2023-07-04 15:45:26 +0300

    add safe component

---
+++ pdp/src/SafeComponent.jsx
@@ -0,0 +1,24 @@
+// NOTE: Another way to handle this would be to fallback to a older version of the component in npm
+
+import React from "react";
+
+export default class SafeComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch() {}
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
