c6dfbb34022d6599f6dbf7c3ba20ba44a9cd63e7
Mourad ElSheraey <mourad.elsheraey@gmail.com>
2023-07-11 15:07:58 +0300

    add pdp content routing

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "home/Header";
 import Footer from "home/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 import "./index.scss";
 import PDPContent from "./PDPContent";
 
 const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
-    <Header />
-      <div className="my-10"><PDPContent /></div>
-    <Footer />
-  </div>
+  <Router>
+    <div className="text-3xl mx-auto max-w-6xl">
+      <Header />
+      <div className="my-10">
+        {/* NOTE: React router version is different from the one used in the tutorial */}
+        <Routes>
+          <Route path="/product/:id" element={<PDPContent />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  </Router>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
